ba1640114fb3cb8993f92b6e0326a46129a2a216
DavidBuchanan314 <d@vidbuchanan.co.uk>
2018-02-08 01:05:08 +0000

    dlopen etc. do not set errno

--- test/test_dlfcn.c
+++ test/test_dlfcn.c
@@ -7,14 +7,14 @@ int main() {
 	printf("Calling dlopen...\n");
 	void * handle = dlopen("./test_dlfcn_hello.nro", RTLD_NOW);
 	if (handle == NULL) {
-		perror("dlopen");
+		printf("dlopen failed\n");
 		return -1;
 	}
 	
 	printf("Calling dlsym...\n");
 	dynamic_hello = dlsym(handle, "hello");
 	if (dynamic_hello == NULL) {
-		perror("dlsym");
+		printf("dlsym failed\n");
 		return -1;
 	}
 	
@@ -24,7 +24,7 @@ int main() {
 	
 	printf("Calling dlclose...\n");
 	if (dlclose(handle) != 0) {
-		perror("dlclose");
+		printf("dlclose failed\n");
 	}
 	
 	printf("dlfcn tests passed, probably.\n");
